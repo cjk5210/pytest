90f86c72174ac25e509937aab40e8484ea693758
changjiakang <332343786@qq.com>
2017-08-14 14:10:20 +0800

    just for test,another version to test

--- testinput.py
+++ testinput.py
@@ -1,3 +1,14 @@
-print("hello world!")
-x = input("please input a string :")
-print(x)
+n = 0
+while n<3:
+    user=raw_input('please enter your name:')
+    password=raw_input('passwd:')
+    if user == 'buer' and password == '1234.com':
+        print('login successful!')
+        while True:
+            shu = raw_input('>>>:')
+            if shu == 'quit':
+                n=3
+                break
+    else:
+        print('login fail')
+        n+=1
