84b566bb6591f7f874ab0330ea3642cb8de03898
chanJackon <332343786@qq.com>
2017-09-11 15:30:55 +0800

    learn some def

--- testDef/def1.py
+++ testDef/def1.py
@@ -13,4 +13,16 @@ def convertToKG(g):
 
 def sanJiao(x,y):
     return math.sqrt(x*x+y*y)
-print(sanJiao(3,4))
+#print(sanJiao(3,4))
+#print(sanJiao(x=4,y=3))
+
+def text_create(name,msg):
+    f_path='c://pythontest/pytest/pytest/testDef/'
+    full_path=f_path+name+'.txt'
+    file=open(full_path,'w')
+    file.write(msg)
+    file.close()
+    print('Done')
+#text_create('helloPython','hello world')
+
+
