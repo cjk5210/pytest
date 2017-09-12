ca2e07b9cb0f503b38e60b9e3fc73c9bbd397fcd
chanJackon <332343786@qq.com>
2017-09-12 16:23:04 +0800

    Change the path for some test code.

--- test1/testinput.py
+++ test1/testinput.py
@@ -1,11 +1,11 @@
 n = 0
 while n<3:
-    user=raw_input('please enter your name:')
-    password=raw_input('passwd:')
+    user=input('please enter your name:')
+    password=input('passwd:')
     if user == 'buer' and password == '1234.com':
         print('login successful!')
         while True:
-            shu = raw_input('>>>:')
+            shu = input('>>>:')
             if shu == 'quit':
                 n=3
                 break
