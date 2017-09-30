4a31798f514cfd5276be4801fb317027bb638d61
changjiakang <332343786@qq.com>
2017-09-30 14:31:02 +0800

    Finish Regex and Start to leanrn operatting file.

---
+++ dataStructure/readAndWriteFile.py
@@ -0,0 +1,5 @@
+
+
+
+# The path of test is 'E:\tensor\pytest\pytest\operateFile'
+
--- dataStructure/testRegex.py
+++ dataStructure/testRegex.py
@@ -5,4 +5,24 @@ def baseTestRegex(str):
     mo=phoneNumRegex.search(str)
     print('Phone number has been found:',mo.group())
 
-baseTestRegex('My number is 415-555-4242')
+#baseTestRegex('My number is 415-555-4242')
+
+def testWildCard():
+    nongreedyRegex=re.compile(r'<.*?>')
+    mo=nongreedyRegex.search('< To serve man> for dinner.>')
+    print(mo.group())
+
+    greedyRegex=re.compile(r'<.*>')
+    mo=greedyRegex.search('< To serve man> for dinner.>')
+    print(mo.group())
+#testWildCard()
+
+def testWildCardMatchWrap():
+    noNelineRegex=re.compile('.*')
+    mo=noNelineRegex.search('Serve the public trust.\nProtect the innocent.\nUphold the law.')
+    print(mo.group())
+    print('-------------------------')
+    newLineRegex=re.compile('.*',re.DOTALL)
+    mo=newLineRegex.search('Serve the public trust.\nProtect the innocent.\nUphold the law.')
+    print(mo.group())
+testWildCardMatchWrap()
